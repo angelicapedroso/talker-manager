57edeea4ee901306cce2963e8f33d2ffba52a736
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-23 11:05:01 -0300

    refactor(loginRoute): add middleware in post /login endpoint

--- src/routes/loginRoute.js
+++ src/routes/loginRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { create } = require('../controllers/loginController');
+const { validateEmail, validatePassword } = require('../middlewares/loginMid');
 
 const router = express.Router();
 
-router.post('/', create);
+router.post('/', validateEmail, validatePassword, create);
 
 module.exports = router;
