8523392d93934635d6a7847a811339ef0296776c
angelicapedroso <angelicapedroso05@gmail.com>
2022-06-23 10:47:05 -0300

    refactor(talkerService): add return of object in create function

--- src/services/talkerService.js
+++ src/services/talkerService.js
@@ -14,32 +14,10 @@ const getId = async (params) => {
 
 const create = async (params = {}) => {
   const talkers = await readTalkerFile();
-  await writeTalkerFile([...talkers, params]);
+  const newTalkers = [...talkers, params];
+  await writeTalkerFile(newTalkers);
+  return params;
 };
-
-//
-//
-//
-// router.post('/', async (req, res) => {
-//   const { name, age, talk } = req.body;
-//   const { authorization } = req.headers;
-//   const talkers = JSON.parse(await fs.readFile(FILE));
-//   const id = talkers.length + 1;
-//   const result = validateAll(authorization, name, age, talk);
-//   const newTalker = {
-//     id,
-//     name,
-//     age,
-//     talk,
-//   };
-
-//   if (result) {
-//     return res.status(result.status).send({ message: result.message });
-//   }
-//   talkers.push(newTalker);
-//   await fs.writeFile(FILE, JSON.stringify(talkers));
-//   return res.status(201).send(newTalker);
-// });
 
 module.exports = {
   getAll,
